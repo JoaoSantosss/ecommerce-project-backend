7f05e04705e39bfa2d46c5326a6bae0baa9f0851
Joao Santos <joaogabriel.silvasantos10@gmail.com>
2024-08-26 22:28:24 -0300

    H2 database configured

--- src/main/java/com/ecommerce/project/models/User.java
+++ src/main/java/com/ecommerce/project/models/User.java
@@ -1,24 +1,38 @@
 package com.ecommerce.project.models;
 
+import java.io.Serializable;
+import java.util.Objects;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "tb_user")
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+public class User implements Serializable{
 	
-	private Integer id;
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
 	private String password;
 	private String name;
 	private String cnpj;
 	private String legaName;
 	private String cpf;
 	
-	public User(Integer id, String password, String name, String cpf) {
+	public User(Long id, String password, String name, String cpf) {
 		super();
 		this.id = id;
 		this.password = password;
@@ -26,13 +40,30 @@ public class User {
 		this.cpf = cpf;
 	}
 
-	public User(Integer id, String password, String name, String cnpj, String legaName) {
+	public User(Long id, String password, String name, String cnpj, String legaName) {
 		super();
 		this.id = id;
 		this.password = password;
 		this.name = name;
 		this.cnpj = cnpj;
 		this.legaName = legaName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
 	}
 	
 	
