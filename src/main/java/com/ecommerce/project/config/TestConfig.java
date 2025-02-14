7f539d4f5f795203cab43a3fac19d64c44ed30c3
Joao Santos <joaogabriel.silvasantos10@gmail.com>
2025-02-14 09:00:04 -0300

    Created a default user to test

--- src/main/java/com/ecommerce/project/config/TestConfig.java
+++ src/main/java/com/ecommerce/project/config/TestConfig.java
@@ -1,5 +1,39 @@
 package com.ecommerce.project.config;
 
-public class TestConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.ecommerce.project.models.User;
+import com.ecommerce.project.models.enums.Role;
+import com.ecommerce.project.repositories.UserRepository;
+
+@Configuration
+public class TestConfig implements CommandLineRunner{
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		User userAdmin = User.builder()
+				.active(true)
+				.email("email@example.com")
+				.name("AdminUser")
+				.password(passwordEncoder.encode("123456"))
+				.phoneNumber("87655443234")
+				.cpf("86546786432")
+				.role(Role.ADMIN)
+				.build();
+		
+		userRepository.save(userAdmin);
+	}
+	
+	
 
 }
